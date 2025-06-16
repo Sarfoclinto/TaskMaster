2e0ea1468e8ba214c280cb26fd2a5cfec4288206
sarfoclinto <wolffighter00000@gmail.com>
2025-06-16 01:33:56 +0000

    feat: update Firebase configuration and add dotenv for environment variables

--- firebase.ts
+++ firebase.ts
@@ -7,5 +7,8 @@ if (!firebaseConfig) {
   throw new Error("Missing Firebase config in app.json");
 }
 
-const app: FirebaseApp = initializeApp(firebaseConfig);
+const app: FirebaseApp = initializeApp({
+  ...firebaseConfig,
+  apiKey: process.env.VITE_APP_API_KEY,
+});
 export default app;
